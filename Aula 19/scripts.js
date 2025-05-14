7486e6778c19c93a1b3611d09a47543db57bd585
Paulo Sergio Deutz Andrião <paulosergioandriao@gmail.com>
2025-05-14 20:38:49 -0300

    Add files via upload

---
+++ Aula 19/scripts.js
@@ -0,0 +1,15 @@
+function dados() {
+
+document.getElementById("teste").innerHTML = "Nome: " + window.prompt("Qual seu nome: ");
+
+document.write.teste;
+
+}
+
+function nome() {
+
+    window.alert("Seu nome é: " + document.getElementById("nome").value)
+
+
+    console.log(nome.value)
+}
