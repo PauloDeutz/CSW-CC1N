b4cdfe522f1d9d47766741179510318415877e58
Paulo Sergio Deutz Andrião <paulosergioandriao@gmail.com>
2025-05-19 20:39:38 -0300

    Add files via upload

---
+++ Aula 20/scripts.js
@@ -0,0 +1,9 @@
+function idade() { 
+    var idadepessoa = document.getElementById("nidade").value;
+    if (idadepessoa >= 1 && idadepessoa <= 11) {
+        window.alert("Você é uma criança")
+    }
+    else {
+        window.alert("asd")
+    }
+}
